21ffd75ebc8459cbaab8b6b12d2225cd3de7606d
Oleg Bukatchuk <oleg@bukatchuk.com>
2019-02-26 14:30:34 +0300

    variable

--- variable.php
+++ variable.php
@@ -1,3 +1,13 @@
 <?php
 
+  $body_temp = 36.6;
+
+  echo $body_temp;
+
+  echo "<p>Температура тела составляет $body_temp градусов Цельсия ";
+
+  $body_temp = 97.88;
+
+  echo " ($body_temp градусов Фаренгейта)</p>";
+
 ?>
