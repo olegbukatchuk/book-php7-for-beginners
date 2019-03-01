6d4c2664ce9c8e59334b8861ce3ee668749f7fc6
Oleg Bukatchuk <oleg@bukatchuk.com>
2019-03-01 13:53:06 +0300

    add arithmetic

---
+++ 03/arithmetic.php
@@ -0,0 +1,24 @@
+<?php
+
+  $a = 5;
+  $b = 2;
+
+  $result = $a + $b;
+  echo "Сложение: $result<br>";
+
+  $result = $a - $b;
+  echo "Вычитание: $result<br>";
+
+  $result = $a * $b;
+  echo "Умножение: $result<br>";
+
+  $result = $a / $b;
+  echo "Деление: $result<br>";
+
+  $result = $a % $b;
+  echo "Деление по модулю: $result<br>";
+
+  $result = $a ** $b;
+  echo "Возведение в степень: $result<br>";
+
+?>
