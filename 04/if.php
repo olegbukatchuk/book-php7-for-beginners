c14273cdcbe601679912eb534d81eca6de4c4445
Oleg Bukatchuk <oleg@bukatchuk.com>
2019-03-13 23:32:31 +0300

    add if & else

---
+++ 04/if.php
@@ -0,0 +1,21 @@
+<?php
+
+  $num = 8;
+
+  if ($num > 5) {
+    echo "$num больше 5<br>";
+  }
+
+  if ($num <= 5) {
+    echo "$num меньше 6<br>";
+  }
+
+  if ($num % 2 == 0) {
+    echo "$num - четное<br>";
+  }
+
+  if ($num % 2 != 0) {
+    echo "$num - не четное<br>";
+  }
+  
+?>
