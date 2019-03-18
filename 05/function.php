980981659073c405e60c22d0d73666361b9814e1
Oleg Bukatchuk <oleg@bukatchuk.com>
2019-03-18 16:08:51 +0300

    add function

---
+++ 05/function.php
@@ -0,0 +1,19 @@
+<?php
+
+function greet() {
+  echo 'Привет, пользователь!<br>';
+}
+
+greet();
+
+function outer() {
+  function inner() {
+    echo 'Вложенная функция вызвана.<br>';
+  }
+  echo 'Вложенная функция создана.<br>';
+}
+
+outer();
+inner();
+
+?>
