703cbaffe3d8aa25eb2821c5ec3bd94cc229d2b4
Oleg Bukatchuk <oleg@bukatchuk.com>
2019-03-18 17:02:23 +0300

    add scope

---
+++ 05/scope.php
@@ -0,0 +1,32 @@
+<?php
+
+  $number = 1;
+
+  echo "Глобальное число: $number<br>";
+
+  function show_local() {
+    $number = 100;
+
+    echo "Локальное число: $number<hr>";
+  }
+
+  show_local();
+
+  function recur() {
+    global $number;
+    static $letter = 'A';
+
+    if ($number < 14) {
+      echo "$number:$letter | ";
+
+      $number++;
+      $letter++;
+      recur();
+    }
+  }
+
+  recur();
+
+  echo "<hr>Глобальное число: $number";
+
+?>
