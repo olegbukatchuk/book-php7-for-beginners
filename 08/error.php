11d46ddada9695252d9c492f8824ac46ddae9d99
Oleg Bukatchuk <oleg@bukatchuk.com>
2019-04-22 20:27:24 +0300

    add error & exception

---
+++ 08/error.php
@@ -0,0 +1,30 @@
+<?php
+
+function error_handler($level, $massage) {
+  switch ($level) {
+    case 2:
+      $str = 'Внимание!';
+      break;
+    case 8:
+      $str = 'Предупреждение!';
+      break;
+    default:
+      $str = 'Ошибка!';
+  }
+
+  echo "<b>$str</b><br>$massage<hr>";
+}
+
+set_error_handler('error_handler');
+
+echo ($var);
+
+$file = fopen('nosuch.txt', 'r');
+
+$number = 2;
+
+if ($number >= 1) {
+  trigger_error('Числовое значение должно быть 1 или меньше');
+}
+
+?>
