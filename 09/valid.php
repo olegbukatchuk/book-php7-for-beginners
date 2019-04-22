ade935832419682ceae41fb6e8c5982179672d70
Oleg Bukatchuk <oleg@bukatchuk.com>
2019-04-22 22:41:14 +0300

    add valid

---
+++ 09/valid.php
@@ -0,0 +1,12 @@
+<form action="valid_handler.php" method="POST">
+  <fieldset>
+    <legend>Введите колчество и одрес email</legend>
+    <p>Количество:
+      <input type="text" name="quantity">
+    </p>
+    <p>Адрес email:
+      <input type="text" name="email">
+    </p>
+  </fieldset>
+  <p><input type="submit" value="Подача запрооса"></p>
+</form>
