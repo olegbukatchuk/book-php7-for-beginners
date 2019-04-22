c7970b7c994005d28cda892e5c1d60f911532547
Oleg Bukatchuk <oleg@bukatchuk.com>
2019-04-22 22:01:39 +0300

    add action & isset

---
+++ 09/action.php
@@ -0,0 +1,11 @@
+<form action="action_handler.php" method="POST">
+  <dl>
+    <dt>Имя:
+    <dd><input type="text" name="name">
+    <dt>Адрес email:
+    <dd><input type="text" name="mail">
+    <dt>Примечание:
+    <dd><textarea rows="5" cols="20" name="comment"></textarea>
+  </dl>
+  <p><input type="submit" value="Подача запроса"></p>
+</form>
---
+++ 09/isset.php
@@ -0,0 +1,12 @@
+<form action="issset_handler.php" method="POST">
+  <fieldset>
+    <legend>К какому типу относится язык PHP?</legend>
+    Структурный
+    <input type="radio" name="definition" value="Структурный"><br>
+    Процедурный
+    <input type="radio" name="definition" value="Процедурный"><br>
+    Объектно-ориентрированный
+    <input type="radio" name="definition" value="Объектно-ориентированный"><br>
+  </fieldset>
+  <p><input type="submit" value="Подача запрооса"></p>
+</form>
