248c5554385c70321ec3dc418d7d93143e671328
Oleg Bukatchuk <oleg@bukatchuk.com>
2019-04-22 19:22:19 +0300

    add readlines

---
+++ 08/readlines.php
@@ -0,0 +1,11 @@
+<?php
+
+$filestream = fopen('manifesto.txt', 'r') or die ('Невозможно открыть файл!');
+
+while (!feof($filestream)) {
+  echo fgets($filestream).'<br>';
+}
+
+fclose($filestream);
+
+?>
